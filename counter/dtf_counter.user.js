6b7f76c40608eb0e04f414cb9059c1ce0e78234c
suvitruf <suvitruf@gmail.com>
2020-09-27 18:30:52 +0300

    raw

--- counter/dtf_counter.user.js
+++ counter/dtf_counter.user.js
@@ -4,8 +4,8 @@
 // @namespace   https://github.com/Suvitruf/dtf-scripts
 // @description Добавляет счётчики на страницу редактирования статьи
 // @author      Suvitruf
-// @updateURL   https://github.com/Suvitruf/dtf-scripts/blob/master/counter/dtf_counter.meta.js
-// @downloadURL https://github.com/Suvitruf/dtf-scripts/blob/master/counter/dtf_counter.user.js
+// @updateURL   https://github.com/Suvitruf/dtf-scripts/raw/master/counter/dtf_counter.meta.js
+// @downloadURL https://github.com/Suvitruf/dtf-scripts/raw/master/counter/dtf_counter.user.js
 // @include     *://*.dtf.ru*
 // @include     *://dtf.ru/*
 // @grant       none
