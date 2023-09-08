0b3b6ec3621bb42240dc4f2b3669d2576448be50
suvitruf <suvitruf@gmail.com>
2023-09-08 19:10:46 +0300

    Counter fixes: - Clear interval - Check url to be able to reinit counter block

--- counter/dtf_counter.meta.js
+++ counter/dtf_counter.meta.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name        dtf counter
-// @version     1
+// @version     2
 // @namespace   https://github.com/Suvitruf/dtf-scripts
 // @description Добавляет счётчики на страницу редактирования статьи
 // @author      Suvitruf
 // @updateURL   https://github.com/Suvitruf/dtf-scripts/raw/master/counter/dtf_counter.meta.js
 // @downloadURL https://github.com/Suvitruf/dtf-scripts/raw/master/counter/dtf_counter.user.js
-// @include     *://*.dtf.ru*
-// @include     *://dtf.ru/*
+// @include     *://*dtf.ru/u/*writing=*
 // @grant       none
 // ==/UserScript==
